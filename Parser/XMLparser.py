e4a1ab755896b80ddc1bfdfcdd87c56ed43a0e85
yxd421 <yxd421@case.edu>
2025-02-03 17:11:26 -0800

    final rev

--- Parser/XMLparser.py
+++ Parser/XMLparser.py
@@ -1,7 +1,6 @@
 import mwxml
 import json
 import argparse
-from tqdm import tqdm  # Import tqdm
 
 def process_page(dump, page):
     for page in dump:
@@ -20,12 +19,9 @@ def main():
     args = parser.parse_args()
 
     with open(args.output_file, "w") as outfile:  # Open in write mode "w" to avoid appending to previous content. If you want to append, use "a".
-        with tqdm(total=None, desc="Processing pages", unit="page") as pbar: # Add tqdm here
-            for item in mwxml.map(process_page, [args.input_file], args.num_processes):
-                for i in item: #item is a generator
-                    json_line = json.dumps(i, ensure_ascii=False) + "\n"  # Ensure UTF-8 encoding
-                    outfile.write(json_line)
-                    pbar.update(1) # update the bar
+        for item in mwxml.map(process_page, [args.input_file], args.num_processes):
+            json_line = json.dumps(item, ensure_ascii=False) + "\n"  # Ensure UTF-8 encoding
+            outfile.write(json_line)
 
     print(f"Successfully processed {args.input_file} and wrote to {args.output_file}")
 
