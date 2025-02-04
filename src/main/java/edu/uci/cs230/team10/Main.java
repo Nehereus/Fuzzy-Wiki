ad3f524ce7a5945c5ef52d8e9c7bed6014d0ec8f
yxd421 <yxd421@case.edu>
2025-02-03 23:31:32 -0800

    add input/output path for the hadoop main

--- src/main/java/edu/uci/cs230/team10/Main.java
+++ src/main/java/edu/uci/cs230/team10/Main.java
@@ -1,8 +1,11 @@
 package edu.uci.cs230.team10;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.lucene.analysis.standard.*;
 import org.apache.lucene.document.*;
@@ -21,7 +24,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "wiki index");
+        JobConf jobConf = new JobConf(conf, Main.class);
+        FileInputFormat.addInputPath(jobConf, new Path(args[0]));
+        FileInputFormat.addInputPath(jobConf, new Path("./output"));
+        Job job = Job.getInstance(jobConf, "wiki index");
         job.setJarByClass(Main.class);
         job.setMapperClass(Parser.class);
         job.setCombinerClass(Indexer.class);
