fb76ceac7a2ba66a0740b2c31a5388d6ac7f492a
yxd421 <yxd421@case.edu>
2025-02-05 23:05:08 -0800

    working on a simple demo of search function

--- src/main/java/edu/uci/cs230/team10/Searcher.java
+++ src/main/java/edu/uci/cs230/team10/Searcher.java
@@ -1,5 +1,6 @@
 package edu.uci.cs230.team10;
 
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
@@ -29,6 +30,7 @@ public class Searcher {
 
     /*a basic fuzzy searcher*/
     public ScoreDoc[] search(String query) throws IOException {
+        logger.info("Searching for: " + query);
         FuzzyQuery fuzzyQuery = new FuzzyQuery(new Term("text", query));
         ScoreDoc[] hits = searcher.search(fuzzyQuery, 10).scoreDocs;
         return hits;
@@ -36,8 +38,10 @@ public class Searcher {
     public static void main(String[] args) throws IOException {
         Searcher searcher = new Searcher();
         ScoreDoc[] hits = searcher.search(args[0]);
+        int i = 0;
         for (ScoreDoc hit : hits) {
-            logger.info("Hit: " + hit.toString());
+            Document d = reader.storedFields().document(hit.doc);
+            System.out.println(i+": " + d.get("title"));
         }
     }
 
